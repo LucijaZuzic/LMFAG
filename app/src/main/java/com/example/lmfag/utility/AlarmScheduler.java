1251d35097a23336bbca0b41d8bc2d3f319acd7b
dominikcondric <domi.condric107@gmail.com>
2022-06-03 11:14:57 +0200

    Added alarm scheduler and alarm receiver

---
+++ app/src/main/java/com/example/lmfag/utility/AlarmScheduler.java
@@ -0,0 +1,17 @@
+package com.example.lmfag.utility;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.lmfag.receivers.EventAlarmReceiver;
+
+public class AlarmScheduler {
+    static void scheduleAlarm(Context applicationContext, int timeInMillis) {
+        AlarmManager alarmManager = (AlarmManager) applicationContext.getSystemService(Context.ALARM_SERVICE);
+        Intent alarmReceiverIntent = new Intent(applicationContext, EventAlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, alarmReceiverIntent, 0);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, timeInMillis - System.currentTimeMillis(), pendingIntent);
+    }
+}
